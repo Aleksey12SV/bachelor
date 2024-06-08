0f586db7a91461cab5a8c83697e73effedaa6498
Aleksey12SV <aleksey12sv@gmail.com>
2024-06-08 12:09:51 +0300

    feat: add internationalization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import './lib/i18n.ts'
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
