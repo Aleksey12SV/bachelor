e7b36ab4281930d0b3b0c34e85cbde0444ae9550
Aleksey12SV <aleksey12sv@gmail.com>
2024-07-08 23:15:20 +0300

    chore: update sql data

--- backend/app/app/src/main/resources/data.sql
+++ backend/app/app/src/main/resources/data.sql
@@ -16,7 +16,26 @@ INSERT INTO property_type (id, name, description) VALUES (2, 'Office', 'Commerci
 
 -- Insert initial data into real_estates table
 INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (1, 1, 250000.00, 85.0, 1);
-INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (2, 2, 500000.00, 150.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (2, 2, 180000.00, 45.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (3, 1, 500000.00, 150.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (4, 2, 200000.00, 65.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (5, 1, 220000.00, 55.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (6, 2, 240000.00, 70.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (7, 1, 600000.00, 180.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (8, 2, 280000.00, 90.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (9, 1, 150000.00, 40.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (10, 2, 230000.00, 75.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (11, 1, 200000.00, 50.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (12, 2, 190000.00, 60.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (13, 1, 270000.00, 95.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (14, 2, 210000.00, 55.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (15, 1, 400000.00, 130.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (16, 2, 260000.00, 80.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (17, 1, 220000.00, 60.0, 1);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (18, 2, 350000.00, 100.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (19, 1, 250000.00, 85.0, 2);
+INSERT INTO real_estates (id, building_id, price, size, property_type_id) VALUES (20, 2, 160000.00, 50.0, 1);
+
 
 -- Insert initial data into sales table
 INSERT INTO sales (id, real_estate_id, sale_date, status, sale_price) VALUES (1, 1, '2023-01-01', 'completed', 240000.00);
