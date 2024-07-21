7da02fe22bb3ca56bb1387eaa8a4e7cfc3a5d3e0
Aleksey12SV <aleksey12sv@gmail.com>
2024-07-22 00:30:15 +0300

    feat: handle SVGs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "tailwindcss";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,5 +16,5 @@ export default defineConfig({
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
-})
+  },
+});
