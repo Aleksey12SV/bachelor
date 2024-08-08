1aeb7f41db2f1b739217af2506c3e671bb7205cd
Aleksey12SV <aleksey12sv@gmail.com>
2024-08-09 00:00:07 +0300

    feat: update models

---
+++ frontend/src/models/Image.ts
@@ -0,0 +1,7 @@
+export type Image = {
+    description?: string,
+    propertyId?: number,
+    buildingId?: number,
+    image64: string,
+    isMainImage?: boolean
+}
--- frontend/src/models/Seller.ts
+++ frontend/src/models/Seller.ts
@@ -1,4 +1,5 @@
 export type Seller = {
+  id: number;
   firstName: string;
   lastName: string;
   phoneNumber: string;
