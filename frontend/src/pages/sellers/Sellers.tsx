cc87fa5e54c598cd7a072b2b60858179eed0b2f4
Aleksey12SV <aleksey12sv@gmail.com>
2024-09-01 17:07:15 +0300

    feat: sellers page adjustments

--- frontend/src/pages/sellers/Sellers.tsx
+++ frontend/src/pages/sellers/Sellers.tsx
@@ -1,4 +1,7 @@
-import { getSellersWithProperties } from "@/api/sellers";
+import { deleteSeller, getSellersWithProperties } from "@/api/sellers";
+import { useKeycloak } from "@/components/auth/KeycloakProvider";
+import { Roles } from "@/components/auth/Roles";
+import ControlButtons from "@/components/common/ControlButtons";
 import {
   Table,
   TableBody,
@@ -8,53 +11,124 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useQuery } from "@tanstack/react-query";
+import { ExtendedSeller } from "@/models/Seller";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import SellersUpdateOverview from "./SellersUpdateOverview";
+import { toast } from "@/hooks/use-toast";
 
 const Sellers = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const [selectedSeller, setSelectedSeller] = useState<ExtendedSeller>();
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const { data: sellers } = useQuery({
     queryKey: ["sellersWithProperties"],
     queryFn: getSellersWithProperties,
     initialData: [],
   });
-  return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">First Name</TableHead>
-          <TableHead>Last Name</TableHead>
-          <TableHead>Phone Number</TableHead>
-          <TableHead>Associated real estates</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {sellers.map((seller) => (
-          <TableRow key={seller.id}>
-            <TableCell className="font-medium">{seller.firstName}</TableCell>
-            <TableCell>{seller.lastName}</TableCell>
-            <TableCell>{seller.phoneNumber}</TableCell>
-            <TableCell>
-              <div className="flex flex-col">
-                {seller.realEstates.map((realEstate) => (
-                  <button
-                    className="flex flex-row gap-4 hover:underline"
-                    onClick={() => navigate(`/property-list/${realEstate.id}`)}
-                  >
-                    <p>
-                      <b>Id:</b> {realEstate.id},
+  const { hasRole } = useKeycloak();
+  const deleteSellerMutation = useMutation({
+    mutationFn: async () => {
+      if (selectedSeller?.id) {
+        await deleteSeller(selectedSeller.id).catch(() =>
+          toast({
+            title: "Deleting Error",
+            description:
+              "The seller is associated with some real estates. Please remove him from the real estates first.",
+          })
+        );
+        await queryClient.invalidateQueries({
+          queryKey: ["sellersWithProperties"],
+        });
+      }
+    },
+  });
 
-                      <b>Title:</b> {realEstate.title}
-                    </p>
-                  </button>
-                ))}
-              </div>
-            </TableCell>
+  return (
+    <div className="flex-auto flex flex-col p-4 gap-4 scrollable">
+      {hasRole([Roles.ADMIN]) && (
+        <ControlButtons
+          onDelete={() =>
+            deleteSellerMutation
+              .mutateAsync()
+              .then(() => setSelectedSeller(undefined))
+          }
+          isDisabled={!selectedSeller}
+          onAdd={() => {
+            setIsEditing(false);
+            setIsAdding(true);
+            setSelectedSeller(undefined);
+          }}
+          onEdit={() => {
+            setIsEditing(true);
+            setIsAdding(false);
+          }}
+          onClose={
+            isAdding || isEditing
+              ? () => {
+                  setIsAdding(false);
+                  setIsEditing(false);
+                }
+              : undefined
+          }
+        />
+      )}
+      {(isAdding || isEditing) && (
+        <SellersUpdateOverview
+          onSubmit={() => {
+            setIsAdding(false);
+            setIsEditing(false);
+          }}
+          selectedSeller={isAdding ? undefined : selectedSeller}
+          isEditing={isEditing}
+        />
+      )}
+      <Table>
+        <TableCaption>A list of all sellers.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>First Name</TableHead>
+            <TableHead>Last Name</TableHead>
+            <TableHead>Phone Number</TableHead>
+            <TableHead>Associated real estates</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {sellers.map((seller) => (
+            <TableRow
+              key={seller.id}
+              className={selectedSeller?.id === seller.id ? "bg-slate-300" : ""}
+              onClick={() => setSelectedSeller(seller)}
+            >
+              <TableCell className="font-medium">{seller.firstName}</TableCell>
+              <TableCell>{seller.lastName}</TableCell>
+              <TableCell>{seller.phoneNumber}</TableCell>
+              <TableCell>
+                <div className="flex flex-col">
+                  {seller.realEstates.map((realEstate) => (
+                    <div key={realEstate.id}>
+                      <button
+                        className="flex flex-row hover:underline"
+                        onClick={() =>
+                          navigate(`/property-list/${realEstate.id}`)
+                        }
+                      >
+                        <p className="font-medium pr-1">Id:</p> {realEstate.id},
+                        <p className="font-medium pl-1">Title:</p>
+                        <p className="pl-1">{realEstate.title}</p>
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
---
+++ frontend/src/pages/sellers/SellersUpdateOverview.tsx
@@ -0,0 +1,118 @@
+import { createSeller, updateSeller } from "@/api/sellers";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ExtendedSeller, Seller } from "@/models/Seller";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+
+const SellersUpdateOverview = ({
+  selectedSeller,
+  isEditing,
+  onSubmit,
+}: {
+  selectedSeller: ExtendedSeller | undefined;
+  isEditing: boolean;
+  onSubmit: () => void;
+}) => {
+  const queryClient = useQueryClient();
+  const form = useForm<Partial<Seller>>({
+    values: {
+      firstName: selectedSeller?.firstName,
+      lastName: selectedSeller?.lastName,
+      phoneNumber: selectedSeller?.phoneNumber,
+    },
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+    },
+  });
+  const saveSellerMutation = useMutation({
+    mutationFn: async (data: Partial<Seller>) => {
+      await createSeller(data).then(() =>
+        queryClient.invalidateQueries({
+          queryKey: ["sellersWithProperties"],
+        })
+      );
+    },
+  });
+  const updateSellerMutation = useMutation({
+    mutationFn: async (data: Partial<Seller> & { id: number }) => {
+      if (selectedSeller?.id !== undefined) {
+        await updateSeller({ ...data, id: selectedSeller.id }).then(() =>
+          queryClient.invalidateQueries({
+            queryKey: ["sellersWithProperties"],
+          })
+        );
+      }
+    },
+  });
+  const handleSubmit = (data: Partial<ExtendedSeller>) => {
+    if (isEditing) {
+      selectedSeller !== undefined &&
+        selectedSeller &&
+        updateSellerMutation.mutateAsync({ ...data, id: selectedSeller.id });
+    } else {
+      saveSellerMutation.mutateAsync(data);
+    }
+    onSubmit();
+    form.reset();
+  };
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="grid grid-cols-2 gap-6 items-end pb-4 border-b"
+        >
+          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default SellersUpdateOverview;
