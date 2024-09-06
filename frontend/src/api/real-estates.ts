054239f645f26f9876981ea2750c528da06a34d7
Aleksey12SV <aleksey12sv@gmail.com>
2024-09-06 17:52:54 +0300

    feat: add getFilteredRealEstates endpoint

--- frontend/src/api/real-estates.ts
+++ frontend/src/api/real-estates.ts
@@ -1,5 +1,7 @@
 import { axiosInstance } from "@/lib/axios";
+import { PaginatedData } from "@/models/PaginatedData";
 import { RealEstate } from "@/models/RealEstate";
+import { FormType } from "@/models/RealEstateForm";
 import { AxiosResponse } from "axios";
 
 export const createRealEstate = (
@@ -17,6 +19,11 @@ export const updateRealEstate = (
 
 export const getRealEstateById = (realEstateId: string): Promise<RealEstate> =>
   axiosInstance.get(`real-estate/${realEstateId}`).then(({ data }) => data);
+
+export const getFilteredRealEstates = (
+  filters: Partial<FormType & { page: number; size: number }>
+): Promise<PaginatedData<RealEstate>> =>
+  axiosInstance.post(`real-estate/filtered`, filters).then(({ data }) => data);
 
 export const deleteRealEstate = (id: number): Promise<void> =>
   axiosInstance.delete(`/real-estate/${id}`).then(({ data }) => data);
