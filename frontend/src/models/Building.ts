bbee3e6af0ffe63ba512628caea9c1cc17ec4914
Aleksey12SV <aleksey12sv@gmail.com>
2024-09-07 10:32:50 +0300

    feat: update building model

--- frontend/src/models/Building.ts
+++ frontend/src/models/Building.ts
@@ -1,11 +1,12 @@
-import { District } from "./District"
+import { District } from "./District";
 
 export type Building = {
-    id: number,
-    district: District,
-    name: string,
-    floors: number,
-    year: number,
-    construction: string,
-    description: string
-}
+  id: number;
+  district: District;
+  name: string;
+  floors: number;
+  year: number;
+  construction: string;
+  descriptionBG: string;
+  descriptionEN: string;
+};
